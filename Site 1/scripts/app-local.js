7bc5cb6acb305a25049194ad14ca594db041df22
AngeloCamillo <angelomc0072@gmail.com>
2026-05-11 20:23:56 -0300

    Java

---
+++ Site 1/scripts/app-local.js
@@ -0,0 +1,14 @@
+const registerForm = document.getElementById("registerForm");
+
+if (registerForm) {
+    registerForm.addEventListener("submit", e => {
+        e.preventDefault();
+        const user = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+        };
+        localStorage.setItem("user", JSON.stringify(user));
+        document.getElementById("message").innerText = "Cadastro Realizado!";
+    });
+}
